0691d42c0bc4fd33aa4b7517d8c23c5645626115
sehong00 <sehong00@gmail.com>
2023-08-16 12:54:42 +0900

    리뷰 페이지 생성

---
+++ src/app/review1/page.tsx
@@ -0,0 +1,241 @@
+import Footer from "@/components/Footer";
+import SearchBox from "@/components/forms/SearchBox";
+
+export default function Home() {
+  return (
+    <main
+      className={`container d-flex justify-content-start flex-column align-items-start`}
+    >
+      <div className={`mt-5`}>
+        <h5 className="fw-bold" style={{ color: "#1F74F4" }}>
+          Truss
+        </h5>
+
+        <a
+          href="https://forms.gle/ypP5dZGZBc3fvx526"
+          className="link-googleform"
+          style={{ fontSize: "50px" }}
+        >
+          <p className="public" style={{ textAlign: "center", opacity: 1 }}>
+            <span>후기 쓰기</span>
+          </p>
+        </a>
+
+        <div className="sc-gtssRu gmjWml">
+          <div className="sc-dlnjPT cuIYFB">
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g opacity="0.8"></g>
+            </svg>
+          </div>
+          <input
+            placeholder="투자자 검색하기"
+            className="sc-hKFyIo bdDYJz"
+            value=""
+          ></input>
+        </div>
+
+        <div className="sc-kfYqjs jpcbJh">
+          <div className="sc-fKgIGh efpGaJ">
+            <h2 className="sc-bCwgka kQzQQm">아주IB</h2>
+            <span
+              style={{
+                display: "block",
+                marginTop: "20px",
+                fontSize: "12px",
+                opacity: 0.5,
+              }}
+            >
+              평균 점수
+            </span>
+            <div className="sc-cxNIbT caQbbb">
+              <span
+                style={{
+                  display: "block",
+                  marginTop: "20px",
+                  fontSize: "12px",
+                  opacity: 0.5,
+                }}
+              >
+                9.0
+              </span>
+              <span
+                style={{
+                  display: "block",
+                  marginTop: "20px",
+                  fontSize: "12px",
+                  opacity: 0.5,
+                }}
+              >
+                / 10
+              </span>
+            </div>
+          </div>
+          <div className="sc-iwaifL gSkynp">
+            <div className="sc-ksluoS jnYGUA">
+              <div className="sc-hBMVcZ liuvoF">
+                <p className="sc-fnVYJo aqTZb">
+                  <span
+                    style={{
+                      display: "block",
+                      marginTop: "20px",
+                      fontSize: "12px",
+                      opacity: 0.5,
+                    }}
+                  >
+                    9
+                  </span>
+                  <span
+                    style={{
+                      display: "block",
+                      marginTop: "20px",
+                      fontSize: "12px",
+                      opacity: 0.5,
+                    }}
+                  >
+                    / 10
+                  </span>
+                </p>
+                <div className="sc-fFSRdu fdffTR">
+                  <p className="sc-bkbjAj gDLPQT">투자 협상 진행</p>
+                </div>
+              </div>
+              <p className="sc-iemXMA iSPPGk">
+                기본적으로 투자와 사업을 둘 다 해본사람 테가 많이 남. 사업과
+                사업자의 입장을 아주 잘 이해하고 있고 어떤식으로든지 도움을 주기
+                위해 그 누구보다 노력함. 성격이 워낙 좋아서 사람이 무조건 좋기만
+                한것처럼 느낄 수 있는데, 필요할땐 엄청 날카로운 질문과 의견을
+                웃으면서 잘 하셔서 놀랄때가 종종있음. 진심을 다해 걱정해주고
+                도와 주는 투자자를 만나보고 싶다면 전수석님 꼭 추천함.
+              </p>
+              <div className="sc-dIvqjp fUbBAh">
+                <button className="sc-dlMBXb hAfEGY">도움이 됩니다</button>
+                <button className="sc-dlMBXb hAfEGY">
+                  별로 도움이 안 됩니다
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="sc-kfYqjs jpcbJh">
+          <div className="sc-fKgIGh efpGaJ">
+            <h2 className="sc-bCwgka kQzQQm">신한금융그룹</h2>
+            <span
+              style={{
+                display: "block",
+                marginTop: "20px",
+                fontSize: "12px",
+                opacity: 0.5,
+              }}
+            >
+              평균 점수
+            </span>
+            <div className="sc-cxNIbT caQbbb">
+              <span
+                style={{
+                  display: "block",
+                  marginTop: "20px",
+                  fontSize: "12px",
+                  opacity: 0.5,
+                }}
+              >
+                9.0
+              </span>
+              <span
+                style={{
+                  display: "block",
+                  marginTop: "20px",
+                  fontSize: "12px",
+                  opacity: 0.5,
+                }}
+              >
+                / 10
+              </span>
+            </div>
+          </div>
+          <div className="sc-iwaifL gSkynp">
+            <div className="sc-ksluoS jnYGUA">
+              <div className="sc-hBMVcZ liuvoF">
+                <p className="sc-fnVYJo aqTZb">
+                  <span
+                    style={{
+                      display: "block",
+                      marginTop: "20px",
+                      fontSize: "12px",
+                      opacity: 0.5,
+                    }}
+                  >
+                    9
+                  </span>
+                  <span
+                    style={{
+                      display: "block",
+                      marginTop: "20px",
+                      fontSize: "12px",
+                      opacity: 0.5,
+                    }}
+                  >
+                    / 10
+                  </span>
+                </p>
+                <div className="sc-fFSRdu fdffTR">
+                  <p className="sc-bkbjAj gDLPQT">투자 협상 진행</p>
+                </div>
+              </div>
+              <p className="sc-iemXMA iSPPGk">
+                일반적인 벤처투자를 많이 하지는 않음. 정말 FI적인 관점에서
+                수익이 날 수 있는 하드웨어 및 전통 산업 등에 주로 투자하는 회사.
+                일반 스타트업에 대해서 실무진들은 충분히 이해할만한 능력이
+                있으나 임원들이 이해력이 없는건지 이해하고 싶지 않은건지 잘
+                모르겠음. 20명 이상 들어오는 IR에서 졸고 자는 경우는 많이 봤으나
+                단 3명이서 하는 미팅에서 대놓고 조는 경우는 여기서 처음봄.
+                실무진들은 꽤 훌륭하고 똑똑한 사람들이라고 생각함.
+              </p>
+              <div className="sc-dIvqjp fUbBAh">
+                <button className="sc-dlMBXb hAfEGY">도움이 됩니다</button>
+                <button className="sc-dlMBXb hAfEGY">
+                  별로 도움이 안 됩니다
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="my-5">
+          <div className="row mt-5">
+            <label className="fw-bold fs-3">
+              문, 샤시 등 자재변경부터 면적, 층수 등 특별요청까지 자세하게
+              알려주세요.
+            </label>
+            <SearchBox
+              rows={7}
+              placeholder={
+                "ex) 문 색깔은 파란색이 좋을 것 같고, 면적은 5평 정도 줄이고 싶습니다. 외장재 콘크리트로 느낌을 살리고 싶습니다."
+              }
+            />
+          </div>
+          <div className="row mt-5">
+            <label className="fw-bold fs-3">
+              트러스나 건축사사무소, 시공사에게 궁금한 것이 있다면 자유롭게
+              물어봐주세요.
+            </label>
+            <SearchBox
+              rows={3}
+              placeholder={
+                "ex) 트러스룰 이용하면 좋은 점이 무엇인지 구체적으로 알고 싶어요."
+              }
+            />
+          </div>
+        </div> */}
+      </div>
+
+      {/* <Footer page={"세부내역 작성"} url={"/service6"} /> */}
+    </main>
+  );
+}
