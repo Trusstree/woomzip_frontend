a8411a48153e649eaeb55de2a7a7aae8d406c6eb
Mindul <pencake33@naver.com>
2023-08-24 03:08:23 +0900

    대소문자 문제 해결

--- src/components/review/Ratinginfo.tsx
+++ src/components/review/Ratinginfo.tsx
@@ -1,7 +1,7 @@
 // 평점 8 / 10
 
 import React from "react";
-import "./RatingInfo.css"; // Import the CSS file
+import "./Ratinginfo.css"; // Import the CSS file
 
 interface RatingInfoProps {
   rating: number; // Optional prop for the rating value
