0be30f66928b171f678ff51ce1997b3db920ce9c
Mindul <pencake33@naver.com>
2023-09-03 16:35:34 +0900

    변수명 업데이트

--- src/app/review3/page.tsx
+++ src/app/review3/page.tsx
@@ -10,35 +10,37 @@ import { isRequired } from "@/utils/validator";
 import { satisfaction } from "@/type/form";
 import TextArea from "@/components/forms/TextArea";
 
-type builder = {
-  name: string;
-  sotong: satisfaction;
+type office = {
+  officeType: string;
+  officeName: string;
+  communication: satisfaction;
   price: satisfaction;
-  sigongResult: satisfaction;
-  dateJunsu: satisfaction;
+  result: satisfaction;
+  keepingDeadline: satisfaction;
   afterService: satisfaction;
   satisfaction: string;
   description: string;
 }
 
-const validate = (values: builder) => {
+const validate = (values: office) => {
   const errors = {
-    name: isRequired(values.name),
-    sotong: isRequired(values.sotong),
+    officeType: isRequired(values.officeType),
+    officeName: isRequired(values.officeName),
+    communication: isRequired(values.communication),
     price: isRequired(values.price),
-    sigongResult: isRequired(values.sigongResult),
-    dateJunsu: isRequired(values.dateJunsu),
+    result: isRequired(values.result),
+    keepingDeadline: isRequired(values.keepingDeadline),
     afterService: isRequired(values.afterService),
     satisfaction: isRequired(values.satisfaction),
     description: isRequired(values.description),
-  };
+  } as office;
 
   return errors;
 };
 
 export default function Home() {
   const router = useRouter();
-  const callback = async (values: builder) => {
+  const callback = async (values: office) => {
     
     const { data,error } = await postOffice({...values, officeType:"시공사"});
     if (error) console.log(error);
@@ -61,30 +63,30 @@ export default function Home() {
           <div className="row g-3">
             {/*이름*/}
             <TextBox
-              name="name"
-              id="name"
+              name="officeName"
+              id="officeName"
               label="이용하셨던 시공사 이름 / 위치를 입력해주세요. (시/군/구)"
-              type="name"
+              type="officeName"
               placeholder="(주)00종합건설/용인시"
               disabled={false}
               readonly={false}
               handleChange={handleChange}
               handleClick={handleClick}
-              value={values['name']}
-              error={errors['name']}
+              value={values['officeName']}
+              error={errors['officeName']}
             />
 
             {/*소통 만족도*/}
             <Radios
-              name="sotong"
-              id="sotong"
+              name="communication"
+              id="communication"
               label="소통 만족도 (상담, 계약, 시공 전반적인 과정에서 시공사와의 소통 만족도입니다.)"
               disabled={false}
               readonly={false}
               handleChange={handleChange}
               handleClick={handleClick}
-              value={values['sotong']}
-              error={errors['sotong']}
+              value={values['communication']}
+              error={errors['communication']}
             >
               {["매우 만족", "만족", "보통", "불만족", "매우 불만족"]}
             </Radios>
@@ -106,30 +108,30 @@ export default function Home() {
 
             {/*시공결과 만족도*/}
             <Radios
-              name="sigongResult"
-              id="sigongResult"
+              name="result"
+              id="result"
               label="시공결과 만족도 (시공의 퀄리티와 마감처리, 하자여부 등 전반적인 결과에 대한 만족도 입니다.)"
               disabled={false}
               readonly={false}
               handleChange={handleChange}
               handleClick={handleClick}
-              value={values['sigongResult']}
-              error={errors['sigongResult']}
+              value={values['result']}
+              error={errors['result']}
             >
               {["매우 만족", "만족", "보통", "불만족", "매우 불만족"]}
             </Radios>
           
             {/*일정준수 만족도*/}
             <Radios
-              name="dateJunsu"
-              id="dateJunsu"
+              name="keepingDeadline"
+              id="keepingDeadline"
               label="일정준수 만족도 (협약한 일정대로 차질 없이 진행되었는지에 대한 평가입니다.)"
               disabled={false}
               readonly={false}
               handleChange={handleChange}
               handleClick={handleClick}
-              value={values['dateJunsu']}
-              error={errors['dateJunsu']}
+              value={values['keepingDeadline']}
+              error={errors['keepingDeadline']}
             >
               {["매우 만족", "만족", "보통", "불만족", "매우 불만족"]}
             </Radios>
@@ -185,7 +187,7 @@ export default function Home() {
             handleSubmit={handleSubmit}
             values={values}
             errors={errors}
-            keys={['name', 'sotong', 'price', 'sigongResult', 'dateJunsu', 'afterService', 'satisfaction', 'description']}
+            keys={['officeName', 'communication', 'price', 'result', 'keepingDeadline', 'afterService', 'satisfaction', 'description']}
             allRequired={true}
           />
         </form>
--- src/app/review4/page.tsx
+++ src/app/review4/page.tsx
@@ -11,35 +11,37 @@ import TextArea from "@/components/forms/TextArea";
 import { postOffice } from "@/api/officeAPI";
 import { alertSuccess } from "@/utils/alertSuccess";
 
-type contructor = {
-  name: string;
-  sotong: satisfaction;
+type office = {
+  officeType: string;
+  officeName: string;
+  communication: satisfaction;
   price: satisfaction;
-  sigongResult: satisfaction;
-  dateJunsu: satisfaction;
+  result: satisfaction;
+  keepingDeadline: satisfaction;
   afterService: satisfaction;
   satisfaction: string;
   description: string;
 }
 
-const validate = (values: contructor) => {
+const validate = (values: office) => {
   const errors = {
-    name: isRequired(values.name),
-    sotong: isRequired(values.sotong),
+    officeType: isRequired(values.officeType),
+    officeName: isRequired(values.officeName),
+    communication: isRequired(values.communication),
     price: isRequired(values.price),
-    sigongResult: isRequired(values.sigongResult),
-    dateJunsu: isRequired(values.dateJunsu),
+    result: isRequired(values.result),
+    keepingDeadline: isRequired(values.keepingDeadline),
     afterService: isRequired(values.afterService),
     satisfaction: isRequired(values.satisfaction),
     description: isRequired(values.description),
-  };
+  } as office;
 
   return errors;
 };
 
 export default function Home() {
   const router = useRouter();
-  const callback = async (values: contructor) => {
+  const callback = async (values: office) => {
     const { data,error } = await postOffice({...values, officeType:"건축사사무소"});
     if (error) console.log(error);
     else console.log(data);
@@ -62,30 +64,30 @@ export default function Home() {
           <div className="row g-3">
             {/*이름*/}
             <TextBox
-              name="name"
-              id="name"
+              name="officeName"
+              id="officeName"
               label="이용하셨던 건축사사무소 이름 / 위치를 입력해주세요. (시/군/구)"
-              type="name"
+              type="officeName"
               placeholder="(주)00건축사사무소/용인시"
               disabled={false}
               readonly={false}
               handleChange={handleChange}
               handleClick={handleClick}
-              value={values['name']}
-              error={errors['name']}
+              value={values['officeName']}
+              error={errors['officeName']}
             />
 
             {/*소통 만족도*/}
             <Radios
-              name="sotong"
-              id="sotong"
+              name="communication"
+              id="communication"
               label="소통 만족도 (상담, 계약, 시공 전반적인 과정에서 건축사사무소와의 소통 만족도입니다.)"
               disabled={false}
               readonly={false}
               handleChange={handleChange}
               handleClick={handleClick}
-              value={values['sotong']}
-              error={errors['sotong']}
+              value={values['communication']}
+              error={errors['communication']}
             >
               {["매우 만족", "만족", "보통", "불만족", "매우 불만족"]}
             </Radios>
@@ -107,30 +109,30 @@ export default function Home() {
 
             {/*시공결과 만족도*/}
             <Radios
-              name="sigongResult"
-              id="sigongResult"
+              name="result"
+              id="result"
               label="시공결과 만족도 (설계의 퀄리티와 요구 반영 여부 등 전반적인 결과에 대한 만족도 입니다.)"
               disabled={false}
               readonly={false}
               handleChange={handleChange}
               handleClick={handleClick}
-              value={values['sigongResult']}
-              error={errors['sigongResult']}
+              value={values['result']}
+              error={errors['result']}
             >
               {["매우 만족", "만족", "보통", "불만족", "매우 불만족"]}
             </Radios>
           
             {/*일정준수 만족도*/}
             <Radios
-              name="dateJunsu"
-              id="dateJunsu"
+              name="keepingDeadline"
+              id="keepingDeadline"
               label="일정준수 만족도 (협약한 일정대로 차질 없이 진행되었는지에 대한 평가입니다.)"
               disabled={false}
               readonly={false}
               handleChange={handleChange}
               handleClick={handleClick}
-              value={values['dateJunsu']}
-              error={errors['dateJunsu']}
+              value={values['keepingDeadline']}
+              error={errors['keepingDeadline']}
             >
               {["매우 만족", "만족", "보통", "불만족", "매우 불만족"]}
             </Radios>
@@ -186,7 +188,7 @@ export default function Home() {
             handleSubmit={handleSubmit}
             values={values}
             errors={errors}
-            keys={['name', 'sotong', 'price', 'sigongResult', 'dateJunsu', 'afterService', 'satisfaction', 'description']}
+            keys={['officeName', 'communication', 'price', 'result', 'keepingDeadline', 'afterService', 'satisfaction', 'description']}
             allRequired={true}
           />
         </form>
