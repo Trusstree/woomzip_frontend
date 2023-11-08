83e7b0aea525c95e62dd57260f612ae99fdd1c19
Mindul <pencake33@naver.com>
2023-11-08 11:16:04 +0900

    build 에러 수정

--- src/components/posts/Editor.tsx
+++ src/components/posts/Editor.tsx
@@ -4,7 +4,6 @@ import 'react-quill/dist/quill.snow.css';
 import { ChangeEvent, useCallback, useMemo, useRef, useState } from 'react';
 import ReactQuill from 'react-quill';
 import * as DOMPurify from "dompurify";
-import AWS from "aws-sdk";
 import useForm from '@/hooks/useForm';
 import SubmitButton from '../forms/SubmitButton';
 import { isRequired } from '@/utils/validator';
@@ -67,49 +66,49 @@ export default function Editor(props: EditorProps) {
 
     
 
-    input.addEventListener("change", async () => {
-      //생성한 s3 관련 설정들
-      const config = {
-        bucketName: process.env.AWS_S3_BUCKETNAME,
-        region: process.env.AWS_S3_REGION,
-        accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_S3_SECRET,
-      };
-      AWS.config.update(config);
+    // input.addEventListener("change", async () => {
+    //   //생성한 s3 관련 설정들
+    //   const config = {
+    //     bucketName: process.env.AWS_S3_BUCKETNAME,
+    //     region: process.env.AWS_S3_REGION,
+    //     accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+    //     secretAccessKey: process.env.AWS_S3_SECRET,
+    //   };
+    //   AWS.config.update(config);
 
-      //이미지를 담아 전송할 file을 만든다
-      const file = input.files?.[0];
-      console.log(file);
-      try {
-        //업로드할 파일의 이름으로 Date 사용
-        const name = Date.now();
+    //   //이미지를 담아 전송할 file을 만든다
+    //   const file = input.files?.[0];
+    //   console.log(file);
+    //   try {
+    //     //업로드할 파일의 이름으로 Date 사용
+    //     const name = Date.now();
         
 
-        //앞서 생성한 file을 담아 s3에 업로드하는 객체를 만든다
-        const upload = new AWS.S3.ManagedUpload({
-          params: {
-            ACL: "public-read",
-            Bucket: process.env.AWS_S3_BUCKETNAME, //버킷 이름
-            Key: `upload/${name}`,
-            Body: file,
-          },
-        });
+    //     //앞서 생성한 file을 담아 s3에 업로드하는 객체를 만든다
+    //     const upload = new AWS.S3.ManagedUpload({
+    //       params: {
+    //         ACL: "public-read",
+    //         Bucket: process.env.AWS_S3_BUCKETNAME, //버킷 이름
+    //         Key: `upload/${name}`,
+    //         Body: file,
+    //       },
+    //     });
 
-        //이미지 업로드 후 곧바로 업로드 된 이미지 url을 가져오기
-        //useRef를 사용해 에디터에 접근한 후 에디터의 현재 커서 위치에 이미지 삽입
+  //       //이미지 업로드 후 곧바로 업로드 된 이미지 url을 가져오기
+  //       //useRef를 사용해 에디터에 접근한 후 에디터의 현재 커서 위치에 이미지 삽입
 
-        const IMG_URL = await upload.promise().then((res) => res.Location);
-        console.log(IMG_URL);
-        // const editor = quillRef.current.getEditor();
-        // const range = editor.getSelection();
+  //       const IMG_URL = await upload.promise().then((res) => res.Location);
+  //       console.log(IMG_URL);
+  //       // const editor = quillRef.current.getEditor();
+  //       // const range = editor.getSelection();
 
-        // // 가져온 위치에 이미지를 삽입한다
-        // editor.insertEmbed(range.index, "image", IMG_URL);
+  //       // // 가져온 위치에 이미지를 삽입한다
+  //       // editor.insertEmbed(range.index, "image", IMG_URL);
 
-      } catch (error) {
-        console.log(error);
-      }
-    });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   });
   };
 
 
