1e7b6820db726a8d7a3eb3c1bb6bd6a538654307
Mindul <pencake33@naver.com>
2024-02-29 21:59:10 +0900

    next-auth 아이디 범위 줄이기...

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -6,16 +6,16 @@ import { getUser, postUser } from "@/apis/userAPI";
 export const authOptions = {
   providers: [
     NaverProvider({
-      clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET,
+      clientId: process.env.NAVER_CLIENT_ID,
+      clientSecret: process.env.NAVER_CLIENT_SECRET,
     }),
     KakaoProvider({
-      clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET,
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
   pages: {
