fe0f9d5b3d37ddfab2a65e90be0f2e1d28ecaa1c
Mindul <pencake33@naver.com>
2024-05-16 21:26:33 +0900

    마이페이지 접속 에러 수정

--- src/components/house/HouseCard.tsx
+++ src/components/house/HouseCard.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { cardPriceText } from "@/lib/stringUtil";
 import Image from "next/image";
@@ -6,10 +6,10 @@ import { useRouter } from "next/navigation";
 import { CSSProperties } from "react";
 
 type MainPagePostProps = {
-  data: any
-  className?: string
-  style?: CSSProperties
-}
+  data: any;
+  className?: string;
+  style?: CSSProperties;
+};
 
 export default function HouseCard(props: MainPagePostProps) {
   const router = useRouter();
@@ -20,46 +20,64 @@ export default function HouseCard(props: MainPagePostProps) {
       <div className="card d-flex flex-column align-items-center w-100">
         <Image
           className="rounded-top-2 w-100 m-0"
-          onClick={()=>{router.push(`/house/${data["house_id"]}`);}}
+          onClick={() => {
+            router.push(`/house/${data["house_id"]}`);
+          }}
           src={data["house_img_url"] || "/blur_image.png"}
           alt={`${data["house_explanation"]}`}
           width={220}
-          height={220} 
-          style={{objectFit:"cover", height:"220px"}}
+          height={220}
+          style={{ objectFit: "cover", height: "220px" }}
           placeholder={"blur"}
-          blurDataURL={"/blur_image.png"} />
-        <div
-          className="card-body w-100 pt-1 px-2 fs-6"
-          style={{height:"230px", backgroundColor:"lightgray"}}>
-          <div className="d-flex justify-content-left mb-2" style={{height:"20px"}}>
-            {data["has_model"]=="1" && <div
-              className="badge me-1 text-white rounded-3 text-center align-self-center"
-              style={{backgroundColor:"#136E11"}}>
-              모델하우스
-            </div>}
-            {data["is_hut"]=="1" && <div
-              className="badge me-1 text-white rounded-3 text-center align-self-center"
-              style={{backgroundColor:"#136E11"}}>
-              농막주택
-            </div>}
+          blurDataURL={"/blur_image.png"}
+        />
+        <div className="card-body w-100 pt-1 px-2 fs-6" style={{ height: "230px", backgroundColor: "lightgray" }}>
+          <div className="d-flex justify-content-left mb-2" style={{ height: "20px" }}>
+            {data["has_model"] == "1" && (
+              <div
+                className="badge me-1 text-white rounded-3 text-center align-self-center"
+                style={{ backgroundColor: "#136E11" }}
+              >
+                모델하우스
+              </div>
+            )}
+            {data["is_hut"] == "1" && (
+              <div
+                className="badge me-1 text-white rounded-3 text-center align-self-center"
+                style={{ backgroundColor: "#136E11" }}
+              >
+                농막주택
+              </div>
+            )}
           </div>
           <h4
             className="card-title fw-bold mb-0 text-nowrap"
-            style={{color:"#101648", overflow:"hidden", textOverflow:"ellipsis"}}>
+            style={{ color: "#101648", overflow: "hidden", textOverflow: "ellipsis" }}
+          >
             {data["house_name"]}
           </h4>
           <div className="d-flex flex-column fw-normal">
             <div className="d-flex justify-content-between">
-              {(data["discount_rate"]>0) && <div className="fs-5 fw-bold" style={{color:"#BD4040"}}>{data["discount_rate"]}%</div>}
-                <div className="ms-auto">
-                  {(data["discount_rate"]>0) && <div className="text-muted text-end text-decoration-line-through m-0 p-0" style={{fontSize:"14px", height:"14px"}}>{cardPriceText(data["base_price"])}</div>}
-                  <div className="fs-6 fw-bold text-end">{cardPriceText(data["final_price"])}</div>
+              {data["discount_rate"] > 0 && (
+                <div className="fs-5 fw-bold" style={{ color: "#BD4040" }}>
+                  {data["discount_rate"]}%
                 </div>
-              
+              )}
+              <div className="ms-auto">
+                {data["discount_rate"] > 0 && (
+                  <div
+                    className="text-muted text-end text-decoration-line-through m-0 p-0"
+                    style={{ fontSize: "14px", height: "14px" }}
+                  >
+                    {cardPriceText(data["base_price"])}
+                  </div>
+                )}
+                <div className="fs-6 fw-bold text-end">{cardPriceText(data["final_price"])}</div>
+              </div>
             </div>
             <div className="d-flex justify-content-between">
               <div className="fw-bold">평수</div>
-              <div>{data["total_floor_area"].toFixed(1)}평</div>
+              <div>{data["total_floor_area"]?.toFixed(1) || "NaN"}평</div>
             </div>
             <div className="d-flex justify-content-between">
               <div className="fw-bold">방</div>
--- src/lib/stringUtil.ts
+++ src/lib/stringUtil.ts
@@ -1,10 +1,13 @@
 export const cardPriceText = (price: number | string) => {
-  let _price = Math.floor(Number(price)); // 만 자리 반올림
+  let _price = Number(price);
+  if (isNaN(_price)) return "NaN원"; // 애초에 숫자가 아니면 에러처리
+  _price = Math.floor(_price); // 소수점은 제거
+
   const il = _price % 10000 > 0 ? _price % 10000 : "";
   _price = Math.floor(_price / 10000);
   const man = _price % 10000 > 0 ? (_price % 10000) + "만" : "";
   _price = Math.floor(_price / 10000);
-  const eok = _price % 10000 > 0 ? (_price % 10000) + "억" : "";
+  const eok = _price % 10000 > 0 ? (_price % 10000) + "억" : ""; // 집은 억 단위까지
   return eok + man + il + "원";
 };
 
