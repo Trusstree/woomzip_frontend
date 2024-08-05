4711e0f001c3b3856fb7214b36f9f3e524419b39
Mindul <pencake33@naver.com>
2024-08-05 21:17:14 +0900

    태그 수정 & robot.ts 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import { AnalyticsHead, AnalyticsBody } from "@/components/Analytics";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import StyledComponentsRegistry from "@/components/app/StyledComponentsRegistry";
 import ContextSession from "@/app/ContextSession";
 import Header from "@/app/Header";
@@ -12,6 +13,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "움집",
+  description: "전원생활의 시작을 함께, 움집",
+  keywords: ["모듈러", "모듈러주택", "움집", "주택", "전원생활"],
   metadataBase: new URL(process.env.NEXT_PUBLIC_CALLBACKURL),
   openGraph: {
     title: "움집",
@@ -33,28 +36,27 @@ export const metadata = {
     type: "website",
   },
 };
-//{ children, params }: { children: React.ReactNode }
-export default function RootLayout(arg) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <head>
-        <AnalyticsHead />
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" />
       </head>
-
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
       <body className={inter.className}>
         <StyledComponentsRegistry>
           <ContextSession>
             <Suspense>
-              <AnalyticsBody />
               <Options />
               <Header />
-              {arg.children}
+              {children}
               <Footer />
             </Suspense>
           </ContextSession>
         </StyledComponentsRegistry>
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
     </html>
   );
 }
