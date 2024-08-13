18487621b9da5bb877296bb1d955637993183937
Mindul <pencake33@naver.com>
2024-08-13 21:11:46 +0900

    Edit 컴포넌트 참조 오류 수정

--- src/app/mypage/[uid]/house/_components/AddHouse.tsx
+++ src/app/mypage/[uid]/house/_components/AddHouse.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { ChangeEvent, ChangeEventHandler, useCallback, useState } from "react";
-import { postHouse } from "@/actions/apis/houseAPI";
-import { alertError, alertSuccess } from "@/lib/alertUtil";
-import { HouseSpecificationComponent } from "@/app/mypage/[uid]/house/_components/HouseSpecificationComponent";
-import { HouseImageComponent } from "@/app/mypage/[uid]/house/_components/HouseImageComponent";
-import { HouseDeliveryComponent } from "@/app/mypage/[uid]/house/_components/HouseDeliveryComponent";
-import { HouseInfoComponent } from "@/app/mypage/[uid]/house/_components/HouseInfoComponent";
-import { HousePriceComponent } from "@/app/mypage/[uid]/house/_components/HousePriceComponent";
-import { usePathname, useRouter } from "next/navigation";
+import { ChangeEvent, ChangeEventHandler, useCallback, useState } from 'react';
+import { postHouse } from '@/actions/apis/houseAPI';
+import { alertError, alertSuccess } from '@/lib/alertUtil';
+import { HouseSpecificationComponent } from '@/app/mypage/[uid]/house/_components/HouseSpecificationComponent';
+import { HouseImageComponent } from '@/app/mypage/[uid]/house/_components/HouseImageComponent';
+import { HouseDeliveryComponent } from '@/app/mypage/[uid]/house/_components/HouseDeliveryComponent';
+import { HouseInfoComponent } from '@/app/mypage/[uid]/house/_components/HouseInfoComponent';
+import { HousePriceComponent } from '@/app/mypage/[uid]/house/_components/HousePriceComponent';
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function AddHouse({ uid }) {
   const pathname = usePathname();
@@ -20,9 +20,7 @@ export default function AddHouse({ uid }) {
   const [specificationInfo, setSpecificationInfo] = useState({});
   const [imageList, setImageList] = useState([]);
 
-  const handleHouse = (
-    e: ChangeEvent<HTMLInputElement>
-  ): ChangeEventHandler<HTMLInputElement> => {
+  const handleHouse = (e: ChangeEvent<HTMLInputElement>): ChangeEventHandler<HTMLInputElement> => {
     setHouseInfo((oldValues) => ({
       ...oldValues,
       [e.target.name]: e.target.value,
@@ -30,9 +28,7 @@ export default function AddHouse({ uid }) {
     return;
   };
 
-  const handleSpecification = (
-    e: ChangeEvent<HTMLInputElement>
-  ): ChangeEventHandler<HTMLInputElement> => {
+  const handleSpecification = (e: ChangeEvent<HTMLInputElement>): ChangeEventHandler<HTMLInputElement> => {
     if (e) e.preventDefault();
     setSpecificationInfo((oldValues) => ({
       ...oldValues,
@@ -43,20 +39,13 @@ export default function AddHouse({ uid }) {
 
   const submitFunction = useCallback(async () => {
     //validate를 위한 부분
-    if (!imageList["representative_image"]) {
-      alertError("이미지 에러!", "대표 이미지가 빠졌어요 ㅠㅠ");
+    if (!imageList['representative_image']) {
+      alertError('이미지 에러!', '대표 이미지가 빠졌어요 ㅠㅠ');
       return;
     }
 
-    if (
-      imageList["external_images"].length +
-        imageList["internal_images"].length <
-      5
-    ) {
-      alertError(
-        "이미지 에러!",
-        "제품 내외부 사진을 합쳐서 5장 이상 채워주세요!"
-      );
+    if (imageList['external_images'].length + imageList['internal_images'].length < 5) {
+      alertError('이미지 에러!', '제품 내외부 사진을 합쳐서 5장 이상 채워주세요!');
       return;
     }
 
@@ -72,13 +61,11 @@ export default function AddHouse({ uid }) {
     const [response, error] = await postHouse(data);
     if (error) {
       console.error(error);
-      alertError("에러!", "뭐가 빠진 게 있나봐요 ㅠㅠ");
+      alertError('에러!', '뭐가 빠진 게 있나봐요 ㅠㅠ');
       return;
     }
 
-    console.log(response);
-
-    alertSuccess(houseInfo["house_name"], "제대로 들어갔어요~");
+    alertSuccess(houseInfo['house_name'], '제대로 들어갔어요~');
     router.push(pathname.slice(0, pathname.length - 6));
   }, [houseInfo, optionInfo, deliveryInfo, specificationInfo, imageList]);
 
@@ -89,11 +76,7 @@ export default function AddHouse({ uid }) {
           집 데이터 추가
         </h1>
         {/* 제품 기본 정보 */}
-        <HouseInfoComponent
-          handleHouse={handleHouse}
-          setHouseInfo={setHouseInfo}
-          houseInfo={houseInfo}
-        />
+        <HouseInfoComponent handleHouse={handleHouse} setHouseInfo={setHouseInfo} houseInfo={houseInfo} />
 
         {/* 배송 */}
         <HouseDeliveryComponent setDeliveryInfo={setDeliveryInfo} />
@@ -114,22 +97,18 @@ export default function AddHouse({ uid }) {
         />
 
         {/* 사진 */}
-        <HouseImageComponent
-          uid={uid}
-          imageList={imageList}
-          setImageList={setImageList}
-        />
+        <HouseImageComponent uid={uid} imageList={imageList} setImageList={setImageList} />
 
         {/* submit */}
         <div
           className="mt-4 py-4 d-flex justify-content-center flex-column"
           style={{
-            borderTopStyle: "solid",
-            borderTopColor: "#101648",
-            borderTopWidth: "2px",
+            borderTopStyle: 'solid',
+            borderTopColor: '#101648',
+            borderTopWidth: '2px',
           }}
         >
-          <div className="mt-2 fw-bold fs-5" style={{ color: "#101648" }}>
+          <div className="mt-2 fw-bold fs-5" style={{ color: '#101648' }}>
             마지막으로 작성 정보를 다시 한 번 확인해주세요.
             <br />
             수정 필요가 있다면 검수 후, 수정 요청드릴 수 있습니다.
@@ -141,13 +120,13 @@ export default function AddHouse({ uid }) {
           <button
             type="button"
             name="submit"
-            style={{ backgroundColor: "#101648" }}
+            style={{ backgroundColor: '#101648' }}
             className={`my-5 px-5 py-3 btn btn-lg rounded-lg text-white fw-bold fs-3`}
             onClick={async () => {
               await submitFunction();
             }}
           >
-            {"등록하기"}
+            {'등록하기'}
           </button>
         </div>
       </div>
