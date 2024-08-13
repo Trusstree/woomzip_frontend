a5c919dcfb6008bdb2858e876d7e1c0ce489d3b3
JunJun <zxcomkt@gmail.com>
2024-08-13 21:04:29 +0900

    헤더 로고 패딩값 추가

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Image from "next/image";
-import SigninButton from "@/components/auth/SigninButton";
-import RouteButton from "@/components/RouteButton";
+import React from 'react';
+import Image from 'next/image';
+import SigninButton from '@/components/auth/SigninButton';
+import RouteButton from '@/components/RouteButton';
 
 type HeaderProps = {};
 
@@ -10,36 +10,36 @@ export default function Header(props: HeaderProps) {
     <div
       className={`navbar navbar-expand-md justify-content-center sticky-top`}
       style={{
-        backgroundColor: "white",
+        backgroundColor: 'white',
       }}
     >
       <div
         className="d-flex justify-content-between w-100"
-        style={{ height: "40px", width: "90%", maxWidth: "1300px" }}
+        style={{ height: '40px', width: '90%', maxWidth: '1300px' }}
       >
-        <RouteButton url="/" className="" style={{ marginLeft: "0", paddingLeft: "0" }}>
-          <Image src={"/woomzip_logo.webp"} alt={"Truss Logo"} width={100} height={26} />
+        <RouteButton url="/" className="" style={{ marginLeft: '0' }}>
+          <Image src={'/woomzip_logo.webp'} alt={'Truss Logo'} width={100} height={26} />
         </RouteButton>
 
         <div className="d-none d-md-flex w-100">
           <div className="d-flex align-items-center me-auto">
-            <RouteButton url="/" style={{ fontSize: "15px", marginBottom: "0px" }}>
+            <RouteButton url="/" style={{ fontSize: '15px', marginBottom: '0px' }}>
               홈
             </RouteButton>
-            <RouteButton url="/community" style={{ fontSize: "15px" }}>
+            <RouteButton url="/community" style={{ fontSize: '15px' }}>
               커뮤니티
             </RouteButton>
             {/* <RouteButton url="/living" style={{ fontSize: "15px" }}>
               살아보기
             </RouteButton> */}
-            <RouteButton url="/house" style={{ fontSize: "15px" }}>
+            <RouteButton url="/house" style={{ fontSize: '15px' }}>
               찾아보기
             </RouteButton>
             {/* <RouteButton url="/planning" style={{ fontSize: "15px" }}>
                 계획하기
               </RouteButton> */}
           </div>
-          <div style={{ textAlign: "right", marginRight: "0", padding: "0" }}>
+          <div style={{ textAlign: 'right', marginRight: '0', padding: '0' }}>
             <SigninButton />
           </div>
         </div>
