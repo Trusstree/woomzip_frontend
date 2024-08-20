453e1565658da18a32bfe1462587b077ba8d5c8e
JunJun <zxcomkt@gmail.com>
2024-08-20 15:33:27 +0900

    swiper 지워버리기~

--- src/components/house/HouseCard.tsx
+++ src/components/house/HouseCard.tsx
@@ -1,11 +1,7 @@
 'use client';
 
 import { cardPriceText } from '@/lib/stringUtil';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
 import Image from 'next/image';
-import 'swiper/css';
-import 'swiper/css/pagination';
 import Link from 'next/link';
 
 export default function HouseCard({ data, className }: { data: any; className?: string }) {
