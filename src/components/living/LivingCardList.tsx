91ce618bb2fb5ca3da3b16ac68f31178a88fe58a
Mindul <pencake33@naver.com>
2024-08-27 00:40:04 +0900

    빌드 중에 리빙 데이터 로그 출력되는 거 없앰..

--- src/components/living/LivingCardList.tsx
+++ src/components/living/LivingCardList.tsx
@@ -1,24 +1,24 @@
-import { getLivings } from "@/actions/apis/livingAPI";
-import LivingCard from "@/components/living/LivingCard";
-import { Suspense } from "react";
+import { getLivings } from '@/actions/apis/livingAPI';
+import LivingCard from '@/components/living/LivingCard';
+import { Suspense } from 'react';
 
 const livingData = [
   {
-    company: "모두가하우징",
-    title: "힐링리버",
-    addr: "강원도 화천군",
+    company: '모두가하우징',
+    title: '힐링리버',
+    addr: '강원도 화천군',
     img: [
-      "https://trussbucketdev.s3.ap-northeast-2.amazonaws.com/test_house/healingRiver1.jpeg",
-      "https://trussbucketdev.s3.ap-northeast-2.amazonaws.com/test_house/healingRiver2.jpeg",
+      'https://trussbucketdev.s3.ap-northeast-2.amazonaws.com/test_house/healingRiver1.jpeg',
+      'https://trussbucketdev.s3.ap-northeast-2.amazonaws.com/test_house/healingRiver2.jpeg',
     ],
     url: `/living/1`,
     // context: "지금까지 5명이 살아봤어요!",
   },
   {
-    company: "탑디자인",
-    title: "자연 속 작은 집",
-    addr: "경기도 이천시",
-    img: ["https://trussbucketdev.s3.ap-northeast-2.amazonaws.com/test_house/topDesign1.png"],
+    company: '탑디자인',
+    title: '자연 속 작은 집',
+    addr: '경기도 이천시',
+    img: ['https://trussbucketdev.s3.ap-northeast-2.amazonaws.com/test_house/topDesign1.png'],
   },
 ];
 
@@ -29,13 +29,13 @@ type LivingListProps = {
 };
 
 async function loadData(): Promise<Array<any>> {
-  "use server";
+  'use server';
   const [data, error] = await getLivings();
   if (error) {
     console.log(error);
     return [];
   }
-  console.log(data.data[0]);
+
   return [data.data[0]];
 }
 
@@ -47,12 +47,12 @@ export default async function LivingCardList(props: LivingListProps) {
       {pavilions?.map((e, i) => (
         <div className="col-md-4 col-lg-3" key={i}>
           <LivingCard
-            company={e["nickname"]}
-            title={e["pavilion_name"]}
-            addr={e["pavilion_addr"]}
+            company={e['nickname']}
+            title={e['pavilion_name']}
+            addr={e['pavilion_addr']}
             img={livingData[i].img}
-            url={e["pavilion_id"] ? `/living/${e["pavilion_id"]}` : `/living/1`}
-            context={`지금까지 ${e["use_count"]}명이 살아봤어요!`}
+            url={e['pavilion_id'] ? `/living/${e['pavilion_id']}` : `/living/1`}
+            context={`지금까지 ${e['use_count']}명이 살아봤어요!`}
           />
         </div>
       ))}
