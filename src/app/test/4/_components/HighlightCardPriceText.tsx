9db5f3fc48ee1027a4338a0cd9f86dc7707b0d7b
Mindul <pencake33@naver.com>
2024-10-09 19:43:33 +0900

    test/4 가격 표기 문제 수정

--- src/app/test/4/_components/HighlightCardPriceText.tsx
+++ src/app/test/4/_components/HighlightCardPriceText.tsx
@@ -2,8 +2,8 @@ export const HighlightCardPriceText = ({ price }: { price: number | string }) =>
   let _price = Number(price);
   if (isNaN(_price)) return <>NaN원</>; // 애초에 숫자가 아니면 에러처리
   if (_price == 0) return <>0원</>; // 0원은 만원 단위가 아니라 제거
-  _price = Math.floor(_price); // 소수점은 제거
 
+  _price = Math.floor(_price / 10000); // 일 단위 제거
   const man = _price % 10000 > 0 ? (_price % 10000) + '만' : '';
   _price = Math.floor(_price / 10000);
   const eok = _price % 10000 > 0 ? (_price % 10000) + '억' : ''; // 집은 억 단위까지
