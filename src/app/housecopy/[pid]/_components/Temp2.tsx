a1ffbf68cd009c0e357363c78398c24dd168dc28
JunJun <zxco_mkt@naver.com>
2025-01-02 18:57:33 +0900

    housecopy, 2번째 템플릿 1차

--- src/app/housecopy/[pid]/_components/Temp2.tsx
+++ src/app/housecopy/[pid]/_components/Temp2.tsx
@@ -6,76 +6,159 @@ export default function Temp2({ imageData }) {
       style={{
         width: '100%',
         backgroundColor: 'black',
-        height: 'calc(-61px + 100svh)',
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        position: 'relative',
+        padding: '100px 0',
       }}
     >
-      <div
-        style={{ width: '100%', position: 'absolute', top: 0, left: 0, height: 'calc(-161px + 100svh)', zIndex: '0' }}
-      >
-        <Image fill src={imageData[2]} alt="pic2" style={{ objectFit: 'cover' }} />
-      </div>
-      <div
-        style={{
-          position: 'absolute',
-          zIndex: '1',
-          bottom: '100px',
-          left: 0,
-          width: '100%',
-          height: '30%',
-          background: 'linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0))',
-        }}
-      />
-      <div
-        style={{
-          position: 'absolute',
-          zIndex: '1',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '20%',
-          background: 'linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0))',
-        }}
-      />
-      <div
-        style={{
-          width: '90%',
-          margin: '0 auto',
-          zIndex: '2',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: '100%',
-          wordBreak: 'keep-all',
-        }}
-      >
-        <div
-          style={{
-            color: '#ffffff',
-            fontSize: 'clamp(30px, 4vw, 45px)',
-            fontWeight: '700',
-            marginTop: '20px',
-            textAlign: 'center',
-          }}
-        >
-          이 곳은 1번 템플릿의 제목이 들어가는 곳입니다. 한줄 추천합니다.
+      <div>
+        <div className="row">
+          <div
+            className="col-md-6 col-12"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              position: 'relative',
+            }}
+          >
+            <div
+              style={{
+                width: '95%',
+                marginLeft: '5%',
+                color: '#ffffff',
+                fontSize: '27px',
+                fontWeight: '700',
+                wordBreak: 'keep-all',
+              }}
+            >
+              이 곳은 2번 템플릿의 첫번째 요소 제목이 들어가는 곳입니다. 두 줄 추천합니다.
+            </div>
+            <div
+              style={{
+                width: '95%',
+                color: '#ffffff',
+                fontSize: '16px',
+                fontWeight: '600',
+                wordBreak: 'keep-all',
+                margin: '30px 0 30px 5%',
+              }}
+            >
+              이 곳은 이 2번 템플릿의 첫번째 요소 설명란입니다. <br /> 제품을 한 번 멋들어지게 설명해보세요. 한 세줄
+              추천합니다. 빠진 내용 없이, 만약 이곳에 거실 관련 설명 글이면 거실을 잘 설명해주시면 됩니다. 잘 아시겠죠?
+              조금 넘어도 괜찮아요. 근데 너무 많으면 안됩니다. 4줄이 적당해요
+            </div>
+          </div>
+          <div className="col-md-6 col-12">
+            <div style={{ width: '100%', aspectRatio: '1', overflow: 'hidden' }}>
+              <Image
+                width={800}
+                height={800}
+                src={imageData[1]}
+                alt={'pic3'}
+                style={{ objectFit: 'cover', height: 'calc(-61px + 100svh)', width: '100%', aspectRatio: '1' }}
+              />
+            </div>
+          </div>
         </div>
-        <div
-          style={{
-            color: '#ffffff',
-            fontSize: 'clamp(16px, 3vw, 19px)',
-            fontWeight: '500',
-            marginBottom: '50px',
-            maxWidth: '800px',
-            margin: '50px auto',
-            textAlign: 'center',
-          }}
-        >
-          이 곳은 이 1번 템플릿의 설명란입니다. <br /> 제품을 한 번 멋들어지게 설명해보세요. 한 세줄 추천합니다. 빠진
-          내용 없이, 만약 이곳에 거실 관련 설명 글이면 거실을 잘 설명해주시면 됩니다. 잘 아시겠죠?
+
+        <div className="row" style={{ margin: '100px 0' }}>
+          <div className="col-md-6 col-12 order-md-1 order-2">
+            <div style={{ width: '100%', aspectRatio: '1', overflow: 'hidden' }}>
+              <Image
+                width={800}
+                height={800}
+                src={imageData[2]}
+                alt={'pic3'}
+                style={{ objectFit: 'cover', height: 'calc(-61px + 100svh)', width: '100%', aspectRatio: '1' }}
+              />
+            </div>
+          </div>
+          <div
+            className="col-md-6 col-12 order-md-2 order-1"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              position: 'relative',
+            }}
+          >
+            <div
+              style={{
+                width: '97%',
+                marginLeft: '3%',
+                color: '#ffffff',
+                fontSize: '27px',
+                fontWeight: '700',
+                wordBreak: 'keep-all',
+              }}
+            >
+              이 곳은 2번 템플릿의 두 번째 요소 제목이 들어가는 곳입니다. 두 줄 추천합니다.
+            </div>
+            <div
+              style={{
+                width: '97%',
+                marginLeft: '3%',
+                color: '#ffffff',
+                fontSize: '16px',
+                fontWeight: '600',
+                wordBreak: 'keep-all',
+                margin: '30px 0 30px 5%',
+              }}
+            >
+              이 곳은 이 2번 템플릿의 첫번째 요소 설명란입니다. <br /> 제품을 한 번 멋들어지게 설명해보세요. 한 세줄
+              추천합니다. 빠진 내용 없이, 만약 이곳에 거실 관련 설명 글이면 거실을 잘 설명해주시면 됩니다. 잘 아시겠죠?
+              조금 넘어도 괜찮아요. 근데 너무 많으면 안됩니다. 4줄이 적당해요
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div
+            className="col-md-6 col-12"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              position: 'relative',
+            }}
+          >
+            <div
+              style={{
+                width: '95%',
+                marginLeft: '5%',
+                color: '#ffffff',
+                fontSize: '27px',
+                fontWeight: '700',
+                wordBreak: 'keep-all',
+              }}
+            >
+              이 곳은 2번 템플릿의 첫번째 요소 제목이 들어가는 곳입니다. 두 줄 추천합니다.
+            </div>
+            <div
+              style={{
+                width: '95%',
+                color: '#ffffff',
+                fontSize: '16px',
+                fontWeight: '600',
+                wordBreak: 'keep-all',
+                margin: '30px 0 30px 5%',
+              }}
+            >
+              이 곳은 이 2번 템플릿의 첫번째 요소 설명란입니다. <br /> 제품을 한 번 멋들어지게 설명해보세요. 한 세줄
+              추천합니다. 빠진 내용 없이, 만약 이곳에 거실 관련 설명 글이면 거실을 잘 설명해주시면 됩니다. 잘 아시겠죠?
+              조금 넘어도 괜찮아요. 근데 너무 많으면 안됩니다. 4줄이 적당해요
+            </div>
+          </div>
+          <div className="col-md-6 col-12">
+            <div style={{ width: '100%', aspectRatio: '1', overflow: 'hidden' }}>
+              <Image
+                width={800}
+                height={800}
+                src={imageData[3]}
+                alt={'pic3'}
+                style={{ objectFit: 'cover', height: 'calc(-61px + 100svh)', width: '100%', aspectRatio: '1' }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
